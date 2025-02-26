b5c4a84f5a92585df37edf11f6d1a70e749519ee
Matthew Travis <Matthew.Travis@brighthr.com>
2025-02-26 07:06:33 +0000

    Feat (Statement Validator Tests): Add test cases for insufficient transactions - need at least two transactions from contiguous months to calculated reoccurring income.

--- test/BankStatementValidatorServiceTests.cs
+++ test/BankStatementValidatorServiceTests.cs
@@ -1,5 +1,4 @@
 ﻿using AffordabilityServiceCore.Concretions;
-using AffordabilityServiceCore.Exceptions;
 using AffordabilityServiceCore.Models;
 using NUnit.Framework;
 
@@ -19,5 +18,20 @@ public class BankStatementValidatorServiceTests
     public void Validate_WhenTransactionsIsEmpty_ReturnsFalse()
     {
         Assert.That(_subject.Validate([]), Is.False);
+    }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    public void Check_WhenInsufficientTransactions_ReturnsFalse(int numberOfTransactions)
+    {
+        var transactions = new List<TenantBankStatementTransaction>();
+
+        for (var i = 0; i < numberOfTransactions; i++)
+        {
+            transactions.Add(new TenantBankStatementTransaction(DateTime.UtcNow, "Test", "Test", 0, TransactionDirection.MoneyIn, 0));
+        }
+
+        Assert.That(_subject.Validate(transactions), Is.False);
     }
 }
