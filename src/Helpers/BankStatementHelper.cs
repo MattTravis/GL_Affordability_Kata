61698c09ab007e6c3f6675fd6693d25ad86a6899
Matthew Travis <Matthew.Travis@brighthr.com>
2025-02-26 12:05:23 +0000

    Feat (Bank Statement Helper): Add implementation for Digest. Duplicated some code from the Bank Statement Validator, time to refactor.

--- src/Helpers/BankStatementHelper.cs
+++ src/Helpers/BankStatementHelper.cs
@@ -6,6 +6,21 @@ public static class BankStatementHelper
 {
     public static decimal Digest(IReadOnlyCollection<TenantBankStatementTransaction> transactions)
     {
-        throw new NotImplementedException();
+        var distinctOrderedMonthlyTransactionTimestamps = transactions
+            .Select(x => x.TransactionMonth)
+            .Distinct()
+            .OrderBy(x => x)
+            .ToList();
+
+        var monthlyIncome = transactions
+            .Where(x => x.Direction == TransactionDirection.MoneyIn)
+            .GroupBy(x => new { x.TransactionType, x.Description })
+            .Select(x => new { Transaction = x, Count = x.Count() });
+
+        // Reoccurring income must occur on each month of the statement
+        var monthlyReoccurringIncome = monthlyIncome
+            .Where(x => x.Count == distinctOrderedMonthlyTransactionTimestamps.Count);
+
+        return monthlyReoccurringIncome.Sum(x => x.Transaction.First().Delta);
     }
 }
