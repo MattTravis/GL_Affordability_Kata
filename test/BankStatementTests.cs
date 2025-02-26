0c2ab9086f6432d7ebee59b228fc8711312260a6
Matthew Travis <Matthew.Travis@brighthr.com>
2025-02-26 12:59:43 +0000

    Feat (Bank Statement Tests): Add test case for bank statement monthly income.

---
+++ test/BankStatementTests.cs
@@ -0,0 +1,27 @@
+﻿using AffordabilityServiceCore.Models;
+using NUnit.Framework;
+
+namespace AffordabilityServiceTests;
+
+public class BankStatementTests
+{
+    private BankStatement _subject;
+
+    [SetUp]
+    public void Setup()
+    {
+        IReadOnlyCollection<TenantBankStatementTransaction> transactions = 
+        [
+            new (DateTime.UtcNow.AddMonths(-1), "Test", "Test", 1, TransactionDirection.MoneyIn, 0),
+            new (DateTime.UtcNow, "Test", "Test", 1, TransactionDirection.MoneyIn, 0)
+        ];
+
+        _subject = new BankStatement(transactions);
+    }
+
+    [Test]
+    public void BankStatement_WhenGivenValidTransactions_HasMonthlyIncome()
+    {
+        Assert.That(_subject.MonthlyIncome, Is.EqualTo(1));
+    }
+}
