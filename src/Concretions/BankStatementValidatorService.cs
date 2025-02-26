f6b7c845d659441d1c0ddca7ff584892f7c8f5f7
Matthew Travis <Matthew.Travis@brighthr.com>
2025-02-26 07:19:35 +0000

    Feat (Bank Statement Service): Refactor transaction minimum count check to use a distinct list of months.

--- src/Concretions/BankStatementValidatorService.cs
+++ src/Concretions/BankStatementValidatorService.cs
@@ -7,14 +7,20 @@ public class BankStatementValidatorService : IBankStatementValidatorService
 {
     public bool Validate(IReadOnlyCollection<TenantBankStatementTransaction> transactions)
     {
-        if (transactions.Count < 2)
+        var distinctOrderedMonthlyTransactionTimestamps = transactions
+            .Select(x => new DateTime(x.Timestamp.Year, x.Timestamp.Month, 1))
+            .Distinct()
+            .OrderBy(x => x)
+            .ToList();
+
+        // Bank statements must cover at least two months, minimum valid transaction count is 2.
+        if (distinctOrderedMonthlyTransactionTimestamps.Count < 2)
         {
             return false;
         }
 
-        // Bank statements must cover at least two months
-
         // Bank statements must cover contiguous months
+        
 
         // Must have at least one reoccurring income source
 
